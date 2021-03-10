7b820c2bdbd6bb333a9a2d11c7ace40c7162a497
ponchanmasimasi <ponsan7.12@icloud.com>
2021-03-10 20:50:43 +0900

    商品詳細表示機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,13 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :postage
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :preparation
+  belongs_to :name
+  
+
 
   with_options presence: true do
     validates :name
