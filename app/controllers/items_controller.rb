967f726e9b02569723b84e23fdcc97b43da775b7
ponchanmasimasi <ponsan7.12@icloud.com>
2021-03-11 20:44:44 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -43,6 +43,12 @@ class ItemsController < ApplicationController
 
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end
+
   private
 
   def item_params
