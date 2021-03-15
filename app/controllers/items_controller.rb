9245f7f6d8f817df79505ff1e0c8714f6af89609
ponchanmasimasi <ponsan7.12@icloud.com>
2021-03-15 11:04:09 +0900

    記述ミスの為、変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,6 +51,6 @@ class ItemsController < ApplicationController
   end
 
   def contributor_confirmation
-    redirect_to root_path if current_user !== @item.user || @item.item_purchase.present?
+    redirect_to root_path if current_user! == @item.user || @item.item_purchase.present?
   end
 end
