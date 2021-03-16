2dc2a1e1c1bc5a8db7ef0f253c9d04add7db859c
ponchanmasimasi <ponsan7.12@icloud.com>
2021-03-16 17:13:36 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 2021_03_12_012539) do
     t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "address", null: false
-    t.string "building_name", null: false
+    t.string "building_name"
     t.string "phone_number", null: false
     t.bigint "item_purchase_id", null: false
     t.datetime "created_at", precision: 6, null: false
